8ef9bf061c9145ae725e61ad86f303e25b93122a
hououkira <hououkiradaze@gmail.com>
2023-09-01 11:16:19 +0800

    基本完成了所有辅助功能

--- index.js
+++ index.js
@@ -1,58 +1,76 @@
-let plusbuttons = document.querySelectorAll(".plus");
-let minusbuttons = document.querySelectorAll(".minus");
-let resetbuttons = document.querySelectorAll(".reset");
-
-let targets = [0, 0, 0, 0, 0, 0]; // 用于按钮是用来提升六种的资源数量还是产能，如果是资源则为0，如果是产能则为1
-
-// 阻止双击事件
-document.addEventListener('dblclick', function(e){
-    e.preventDefault();
-});
-
 $(document).ready(function() {
     console.log("loaded");
-  });
+    $('.num-input').on('keydown blur', function(e) {
+        if (e.type === 'keydown' && e.keyCode !== 13) return;
+        e.preventDefault();
+        if(/^[1-9]\d*(\.\d+)?$/.test($(this).val()))
+            $('#credit .num').text($(this).val());
+            $(this).val('');
+    });
+});
 
 
-function increment(button) {
-    if(targets[0] == 0){
-        var $h1 = $(button).parent().parent().prev().find('.num');
-        var currentCount = parseInt($h1.text(), 10);
-        $h1.text(currentCount + 1);
-    }
-    else{
-        var $h1 = $(button).parent().parent().prev().find('.production');
-        var currentCount = parseInt($h1.text(), 10);
-        $h1.text(currentCount + 1);
+function increment(button, type) {
+    switch(type) {
+        case 'num': {
+            let $h1 = $(button).parent().parent().prev().find('.flex').find('.num');
+            let currentCount = parseInt($h1.text(), 10);
+            $h1.text(currentCount + 1);
+        } break;
+        case 'production': {
+            let $h1 = $(button).parent().parent().prev().find('.flex').find('.production');
+            let currentCount = parseInt($h1.text(), 10);
+            $h1.text(currentCount + 1);
+        } break;
     }
 }
   
 
-function decrement(button) {
-    if(targets[0] == 0){
-        var $h1 = $(button).parent().parent().prev().find('.num');
-        var currentCount = parseInt($h1.text(), 10);
-        $h1.text(currentCount - 1);
-    }
-    else{
-        var $h1 = $(button).parent().parent().prev().find('.production');
-        var currentCount = parseInt($h1.text(), 10);
-        $h1.text(currentCount - 1);
+function decrement(button, type) {
+    switch(type) {
+        case 'num': {
+            let $h1 = $(button).parent().parent().prev().find('.flex').find('.num');
+            let currentCount = parseInt($h1.text(), 10);
+            if(currentCount > 0) $h1.text(currentCount - 1);
+        } break;
+        case 'production': {
+            let $h1 = $(button).parent().parent().prev().find('.flex').find('.production');
+            let currentCount = parseInt($h1.text(), 10);
+            if($(button).parent().parent().parent().attr('id') === 'credit') {
+                if(currentCount > -5) $h1.text(currentCount - 1);
+            }
+            else {
+                if(currentCount > 0) $h1.text(currentCount - 1);
+            }
+        } break;
     }
 }
   
 
-function reset(button) {
-    if(targets[0] == 0){
-        var $h1 = $(button).parent().parent().prev().find('.num');
-        $h1.text(0);
-    }
-    else{
-        var $h1 = $(button).parent().parent().prev().find('.production');
-        $h1.text(0);
+function reset(button, type) {
+    switch(type) {
+        case 'num': {
+            if ($(button).parent().parent().parent().attr('id') === 'TR') {
+                $(button).parent().parent().prev().find('.flex').find('.num').text(20);
+            }
+            else $(button).parent().parent().prev().find('.flex').find('.num').text(0);
+        } break;
+        case 'production': {
+            $(button).parent().parent().prev().find('.flex').find('.num').text(1);
+        } break;
     }
 }
 
-function toggleTarget(){
-    
+function production() {
+    $('.num').each(function() { 
+        if($(this).parent().parent().parent().attr('id') === 'TR') return;
+        num = $(this).text();
+        productionValue = $(this).next().text();
+        $(this).text(parseInt(num, 10) + parseInt(productionValue, 10));
+        if($(this).parent().parent().parent().attr('id') === 'credit') {
+            TR = $('#TR').find('.num').text();
+            $(this).text(parseInt(num, 10) + parseInt(productionValue, 10) + parseInt(TR, 10));
+        }
+    })
 }
+
